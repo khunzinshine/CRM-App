8820765c0a26fb5ca04016a0f84a22fd2b5b0d3d
khunzinshine <khunzinshine@ayainnovation.com>
2022-08-28 02:56:27 +0630

    add customer list component

--- src/views/customer-list/CustomerList.js
+++ src/views/customer-list/CustomerList.js
@@ -1,32 +1,35 @@
 import React from "react";
+import SideBar from "../../components/sidebar/SideBar";
 import "./CustomerList.css";
 
 const CustomerList = () => {
   return (
-    <div style={{ overflowX: "auto" }}>
-      <table>
-        <tr>
-          <th>Photo</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Birthday</th>
-          <th>Gender</th>
-          <th>NRC</th>
-          <th>CreatedAt </th>
-          <th>Action</th>
-        </tr>
-        <tr>
-          <td>Jill</td>
-          <td>Smith</td>
-          <td>50</td>
-          <td>50</td>
-          <td>50</td>
-          <td>50</td>
-          <td>50</td>
-          <td>50</td>
-        </tr>
-      </table>
-    </div>
+    <SideBar>
+      <div style={{ overflowX: "auto" }}>
+        <table>
+          <tr>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Birthday</th>
+            <th>Gender</th>
+            <th>NRC</th>
+            <th>CreatedAt </th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>Jill</td>
+            <td>Smith</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+            <td>50</td>
+          </tr>
+        </table>
+      </div>
+    </SideBar>
   );
 };
 
