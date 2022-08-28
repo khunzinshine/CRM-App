e49df0fd41d8fbd67bc102e1bcc6bd7a70f2b7b0
khunzinshine <khunzinshine@ayainnovation.com>
2022-08-28 23:35:53 +0630

    fixed react.dom deprecated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import { AuthContextProvider } from "./context/authContext";
+import { createRoot } from "react-dom/client";
+import store from "./features/index";
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </AuthContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
