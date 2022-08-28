fcf79b05057dd6f66ee9abe9cd139487dca25ccf
khunzinshine <khunzinshine@ayainnovation.com>
2022-08-28 23:37:30 +0630

    add endpoints

---
+++ src/constant/apiEndPoints.js
@@ -0,0 +1,6 @@
+export const ApiEndPoints = {
+  customerList: process.env.REACT_APP_API_SERVER + "/users",
+  updateCustomer: process.env.REACT_APP_API_SERVER + "/users",
+  deleteCustomer: process.env.REACT_APP_API_SERVER + "/users",
+  getDetailCustomer: process.env.REACT_APP_API_SERVER + "/users",
+};
